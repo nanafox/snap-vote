609f2fe99f343ede5f152d60a8abb3088ae3411d
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2025-09-03 20:09:38 +0000

    chore: remove unnecessary Babel presets and dependencies from package.json

--- jest.config.js
+++ jest.config.js
@@ -1,18 +1,19 @@
-/** @type {import('jest').Config} */
-const config = {
-  testEnvironment: "jsdom",
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const nextJest = require("next/jest");
+
+const createJestConfig = nextJest({
+  // Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+  dir: "./",
+});
+
+// Add any custom config to be passed to Jest
+const customJestConfig = {
   setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
-  testPathIgnorePatterns: ["<rootDir>/node_modules/", "<rootDir>/.next/"],
-  transform: {
-    "^.+\\.(js|jsx|ts|tsx)$": "babel-jest",
-  },
+  testEnvironment: "jsdom",
   moduleNameMapper: {
     "^@/(.*)$": "<rootDir>/$1",
-    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
   },
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
-  collectCoverageFrom: ["components/**/*.{js,jsx,ts,tsx}", "!**/*.d.ts", "!**/node_modules/**"],
-  transformIgnorePatterns: ["/node_modules/(?!.*\\.mjs$)"],
 };
 
-module.exports = config;
+// createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
+module.exports = createJestConfig(customJestConfig);
